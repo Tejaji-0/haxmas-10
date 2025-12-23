9e5c3e6e1fe544c42cdf17448e57d7b5404b5d9a
Tejaji-0 <psntejaji@gmail.com>
2025-12-23 07:24:16 +0530

    try beating my highscore of 500

---
+++ conf.lua
@@ -0,0 +1,7 @@
+function love.conf(t)
+    t.title = "shake it and rotate it" -- change this to your game name
+    t.version = "11.4"
+    t.window.width = 600
+    t.window.height = 600
+    t.window.resizable = false
+end
